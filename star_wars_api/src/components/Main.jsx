a32299dd79f294753e7e2f9e51e3372fd362ee5d
jslempa <jslempa@gmail.com>
2024-06-15 11:45:04 -0400

    started other routes

--- star_wars_api/src/components/Main.jsx
+++ star_wars_api/src/components/Main.jsx
@@ -1,5 +1,10 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
+import PeopleList from './PeopleList'
+import PlanetsList from './PlanetsList'
+import FilmsList from './FilmsList'
+import SpeciesList from './SpeciesList'
+import VehiclesList from './VehiclesList'
 import StarshipList from './StarshipList'
 
 const Main = (props) => {
--- star_wars_api/src/components/Nav.jsx
+++ star_wars_api/src/components/Nav.jsx
@@ -9,6 +9,26 @@ const Nav = (props) => {
             <h2>Home</h2>
           </Link>
 
+          <Link to='/people'>
+            <h2>People</h2>
+          </Link>
+
+          <Link to='/planets'>
+            <h2>Planets</h2>
+          </Link>
+
+          <Link to='/films'>
+            <h2>Films</h2>
+          </Link>
+
+          <Link to='/species'>
+            <h2>Species</h2>
+          </Link>
+
+          <Link to='/vehicles'>
+            <h2>Vehicles</h2>
+          </Link>
+
           <Link to='/starships'>
             <h2>Starships</h2>
           </Link>
