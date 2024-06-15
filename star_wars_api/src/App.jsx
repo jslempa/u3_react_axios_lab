4fb6efc970aeb8c92481a5a9e4a9721e47d6b2b5
jslempa <jslempa@gmail.com>
2024-06-15 11:31:35 -0400

    finished App.jsx with the other api calls

--- star_wars_api/src/App.jsx
+++ star_wars_api/src/App.jsx
@@ -6,9 +6,44 @@ import Main from './components/Main'
 import './App.css'
 
 function App() {
+  const [people, setPeople] = useState([])
+  const [planets, setPlanets] = useState([])
+  const [films, setFilms] = useState([])
+  const [species, setSpecies] = useState([])
+  const [vehicles, setVehicles] = useState([])
   const [starships, setStarships] = useState([])
 
   useEffect(() => {
+    const getPeople = async () => {
+      const res = await axios.get(`${BASE_URL}/people`)
+      setPeople(res.data.results)
+    }
+    getPeople()
+
+    const getPlanets = async () => {
+      const res = await axios.get(`${BASE_URL}/planets`)
+      setPlanets(res.data.results)
+    }
+    getPlanets()
+
+    const getFilms = async () => {
+      const res = await axios.get(`${BASE_URL}/films`)
+      setFilms(res.data.results)
+    }
+    getFilms()
+
+    const getSpecies = async () => {
+      const res = await axios.get(`${BASE_URL}/species`)
+      setSpecies(res.data.results)
+    }
+    getSpecies()
+
+    const getVehicles = async () => {
+      const res = await axios.get(`${BASE_URL}/vehicles`)
+      setVehicles(res.data.results)
+    }
+    getVehicles()
+
     const getStarships = async () => {
       const res = await axios.get(`${BASE_URL}/starships`)
       setStarships(res.data.results)
@@ -21,7 +56,12 @@ function App() {
   return (
     <div>
       <Header/>
-      <Main starships = {starships}/>
+      <Main people = {people}
+            planets = {planets}
+            films = {films}
+            species = {species}
+            vehicles = {vehicles}
+            starships = {starships}/>
     </div>
   )
 }
